cfb24c9534049139da1d89b377e71569a8530445
plbb-admin <delfinek11a@gmail.com>
2024-10-24 13:05:19 +0200

    Zaktualizowano zadanie (dodano TryParse, zapętlono program, dodano wymaganie wciśnięcia klawisza aby zamknąć program)

--- csPraktyki1/zadPiotr/Program.cs
+++ csPraktyki1/zadPiotr/Program.cs
@@ -7,8 +7,18 @@ namespace csPraktyki1
         static void Main(string[] args)
         {
             //Aplikacja konsoli C#, która pobiera od użytkownika liczbę całkowitą i sprawdza czy jest ona dodatnia, ujemna czy zerowa
-            Console.WriteLine("Podaj liczbę całkowitą");
-            int number = int.Parse(Console.ReadLine());
+            bool isNumberValid = false;
+            int number = 0;
+            while (!isNumberValid)
+            {
+                Console.WriteLine("Podaj liczbę całkowitą: ");
+                string input = Console.ReadLine();
+                isNumberValid = int.TryParse(input, out number);
+                if (!isNumberValid)
+                {
+                    Console.WriteLine("Nieprawidłowy format. Spróbuj ponownie.");
+                }
+            }
             if (number > 0)
             {
                 Console.WriteLine("Liczba dodatnia");
@@ -21,6 +31,8 @@ namespace csPraktyki1
             {
                 Console.WriteLine("Liczba ujemna");
             }
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć program.");
+            Console.ReadKey();
         }
     }
 }
