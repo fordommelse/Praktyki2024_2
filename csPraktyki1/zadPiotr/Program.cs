cdedaa6ad5632b5c712dd38454997c6ea19108ae
plbb-admin <delfinek11a@gmail.com>
2024-10-30 18:09:11 +0100

    zabezpieczono program

--- csPraktyki1/zadPiotr/Program.cs
+++ csPraktyki1/zadPiotr/Program.cs
@@ -6,33 +6,39 @@ namespace csPraktyki1
     {
         static void Main(string[] args)
         {
-            //Aplikacja konsoli C#, która pobiera od użytkownika liczbę całkowitą i sprawdza czy jest ona dodatnia, ujemna czy zerowa
-            bool isNumberValid = false;
-            int number = 0;
-            while (!isNumberValid)
+            bool continueProgram;
+            do
             {
-                Console.WriteLine("Podaj liczbę całkowitą: ");
-                string input = Console.ReadLine();
-                isNumberValid = int.TryParse(input, out number);
-                if (!isNumberValid)
+                //Aplikacja konsoli C#, która pobiera od użytkownika liczbę całkowitą i sprawdza czy jest ona dodatnia, ujemna czy zerowa
+                bool isNumberValid = false;
+                int number = 0;
+                while (!isNumberValid)
                 {
-                    Console.WriteLine("Nieprawidłowy format. Spróbuj ponownie.");
+                    Console.WriteLine("Podaj liczbę całkowitą: ");
+                    string input = Console.ReadLine();
+                    isNumberValid = int.TryParse(input, out number);
+                    if (!isNumberValid)
+                    {
+                        Console.WriteLine("Nieprawidłowy format. Spróbuj ponownie.");
+                    }
                 }
-            }
-            if (number > 0)
-            {
-                Console.WriteLine("Liczba dodatnia");
-            }
-            else if (number == 0)
-            {
-                Console.WriteLine("Liczba równa 0");
-            }
-            else
-            {
-                Console.WriteLine("Liczba ujemna");
-            }
-            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć program.");
-            Console.ReadKey();
+                if (number > 0)
+                {
+                    Console.WriteLine("Liczba dodatnia");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine("Liczba równa 0");
+                }
+                else
+                {
+                    Console.WriteLine("Liczba ujemna");
+                }
+                Console.WriteLine("Czy chcesz sprawdzić inną liczbę? (t/n)");
+                string response = Console.ReadLine();
+                continueProgram = response.ToLower() == "t";
+            }while (continueProgram);
+            Console.WriteLine("Program zakończony.");
         }
     }
 }
